a756b839174794d21f86be219f82da6169c9133e
EC2 Default User <ec2-user@ip-192-201-21-29.eu-west-1.compute.internal>
2019-11-13 17:54:09 +0000

    exercice des listes dans listes

--- boucles/challenge.py
+++ boucles/challenge.py
@@ -2,4 +2,10 @@
 
 rows_ancien = ["jordan,65", "yaounde,56", "biden,6563", "trump,565", "family,879", "russia,532", "pelosi,2345", "harris,7914", "kamala,1453", "testons,6669", "bill,65"]
 
-w
+int_dep_populations = []
+
+for row in rows_ancien:
+    jordan_row = row.split(",")
+    nathan_row = int(jordan_row[1])
+    int_dep_populations.append(nathan_row)
+print(int_dep_populations)
