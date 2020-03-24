c4d1c4c76a81c689ada88aa7b9c9312b49d5d164
jordanyoumbi <30660122+jordanyoumbi@users.noreply.github.com>
2020-03-24 11:33:47 +0100

    Update calcul.py

--- variables/calcul.py
+++ variables/calcul.py
@@ -6,3 +6,40 @@ print(squared)
 #Division
 divide_france = square / 2
 print(divide_france)
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: xmk-admin-private
+  labels:
+    app: xmk-admin-private
+spec:
+  replicas: 2
+  selector:
+    matchLabels:
+      app: xmk-admin-private
+  template:
+    metadata:
+      labels:
+        app: xmk-admin-private
+    spec:
+      containers:
+      - name: xmk-admin-private
+#        image: nginx:1.14.2
+        image: de.icr.io/xmk-admin/xmk-admin:48-20200320215821
+        ports:
+        - containerPort: 80
+---
+apiVersion: v1
+kind: Service
+metadata:
+ name: xmk-admin-lb-private
+ annotations:
+   service.kubernetes.io/ibm-load-balancer-cloud-provider-ip-type: private
+spec:
+ type: LoadBalancer
+ selector:
+   app: xmk-admin-private
+ ports:
+  - protocol: TCP
+    port: 80
+    targetPort: 80
